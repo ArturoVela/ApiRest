a4f0b6914dbed303d0e91bba6fd5c561f43388d9
Arturo Vela <velaarturo70@gmail.com>
2025-04-16 17:59:16 -0500

    Mejoras

--- src/main/java/api/rest/fisi/api/security/JwtFilter.java
+++ src/main/java/api/rest/fisi/api/security/JwtFilter.java
@@ -1,5 +1,37 @@
 package api.rest.fisi.api.security;
 
-public class JwtFilter {
+import java.util.ArrayList;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.GenericFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class JwtFilter extends GenericFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+                HttpServletRequest request = (HttpServletRequest) req;
+                String header = request.getHeader("Authorization");
+                if (header != null && header.startsWith("Bearer ")) {
+                    String token = header.substring(7);
+                    if (jwtUtil.validadeToken(token)) { // Usar la instancia inyectada
+                        String clienteId = jwtUtil.extraerClienteId(token);
+                        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(clienteId, null, new ArrayList<>());
+                        SecurityContextHolder.getContext().setAuthentication(auth);
+                    }
+                }
+                chain.doFilter(req, res);
+    }
 
 }
